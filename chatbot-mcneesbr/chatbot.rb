8a9369c3b8cc844fd1a67bea8a00e4fcadb0ff8a
Ben McNees <mcneesbr@rose-hulman.edu>
2011-05-02 23:09:23 -0400

    Implemented the last scenario

--- chatbot-mcneesbr/chatbot.rb
+++ chatbot-mcneesbr/chatbot.rb
@@ -2,3 +2,13 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts 'Hello, ' + name + '!'
+puts 'Choose a war game:'
+puts '1. Total Annihilation'
+puts '2. World War 3'
+puts '3. War Against the Machines'
+puts '4. Global Thermonuclear War'
+puts '5. Defcon 1'
+num = gets.chomp
+if num == "4"
+puts 'BOOM!'
+end
